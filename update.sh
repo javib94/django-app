036eeb8d16c214bc70c34aeacc02a8d5d3a50994
Javier Barreda <javier.barreda94@gmail.com>
2018-07-10 12:35:03 -0600

    role ok

--- update.sh
+++ update.sh
@@ -5,3 +5,6 @@ aws ecr get-login --no-include-email --region us-east-2
 docker build -t ecs-javi-repository ./src
 docker tag ecs-javi-repository:latest 797409686075.dkr.ecr.us-east-2.amazonaws.com/ecs-javi-repository:latest
 docker push 797409686075.dkr.ecr.us-east-2.amazonaws.com/ecs-javi-repository:latest
+
+
+#HOLA MUNDO 
