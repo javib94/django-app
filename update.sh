77d2a98ba7749250037d20473f92dedf0df5a294
Javier Barreda <javier.barreda94@gmail.com>
2018-07-10 12:44:56 -0600

    update ecr try

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-eval $(aws ecr get-login --no-include-email --region us-east-2 | sed 's|https://||')
-aws ecr get-login --no-include-email --region us-east-2 
+#eval $(aws ecr get-login --no-include-email --region us-east-2 | sed 's|https://||')
+#aws ecr get-login --no-include-email --region us-east-2 
 sudo docker build -t ecs-javi-repository ./src
 sudo docker tag ecs-javi-repository:latest 797409686075.dkr.ecr.us-east-2.amazonaws.com/ecs-javi-repository:latest
 sudo docker push 797409686075.dkr.ecr.us-east-2.amazonaws.com/ecs-javi-repository:latest
