31a5b5eedba0d764e3aeedc0e7f09f8d7f860c81
Javier Barreda <javier.barreda94@gmail.com>
2018-07-10 12:50:39 -0600

    jenkins sudo

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #eval $(aws ecr get-login --no-include-email --region us-east-2 | sed 's|https://||')
-#aws ecr get-login --no-include-email --region us-east-2 
-echo $USER
-#sudo docker build -t ecs-javi-repository ./src
-#sudo docker tag ecs-javi-repository:latest 797409686075.dkr.ecr.us-east-2.amazonaws.com/ecs-javi-repository:latest
-#sudo docker push 797409686075.dkr.ecr.us-east-2.amazonaws.com/ecs-javi-repository:latest
+aws ecr get-login --no-include-email --region us-east-2 
+#echo $USER
+sudo docker build -t ecs-javi-repository ./src
+sudo docker tag ecs-javi-repository:latest 797409686075.dkr.ecr.us-east-2.amazonaws.com/ecs-javi-repository:latest
+sudo docker push 797409686075.dkr.ecr.us-east-2.amazonaws.com/ecs-javi-repository:latest
 
 
 #HOLA MUNDO 
